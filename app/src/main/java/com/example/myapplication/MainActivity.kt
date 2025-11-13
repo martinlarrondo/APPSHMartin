bcfea0c74b08a7870c4974ab852b003bbc7c7e44
martinlarrondo <mblarrondo@gmail.com>
2025-11-13 17:06:34 -0300

    firebase conectado
    se completa la instalacion de fire base y que conectado correctamente y funcional para la app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import com.google.firebase.FirebaseApp
 import com.example.myapplication.NavController.NavController
 import android.util.Log
-import com.google.firebase.FirebaseApp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
